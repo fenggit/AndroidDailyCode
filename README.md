ba238d383b71490d8b3d1baa5956e402a5868c0a
hefeng <fenggit@163.com>
2021-12-17 18:13:22 +0800

    mvp

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # AndroidDailyCode
+
+## Android 框架
+
+### MVC
+
+### MVP
+architecture-mvp : MVP 架构代码
+
+> https://www.bilibili.com/video/BV1JQ4y1X7QE?p=5
+> 
+### MVVM
+
+### MVI
---
+++ app/src/main/java/com/androiddaily/code/architecture/ArchitectureItemRecyclerViewAdapter.kt
@@ -0,0 +1,51 @@
+package com.androiddaily.code.architecture
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.androiddaily.code.R
+
+import com.androiddaily.code.architecture.placeholder.PlaceholderContent.PlaceholderItem
+import com.androiddaily.code.architecture.databinding.FragmentArchitectureBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class ArchitectureItemRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<ArchitectureItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentArchitectureBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentArchitectureBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
